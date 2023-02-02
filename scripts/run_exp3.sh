e6d7a6b7c7559fcb1257bca3f757f38b0170f061
MohanadOdema <mohanad.odema@gmail.com>
2023-02-02 12:45:45 -0800

    exp3

--- scripts/run_exp3.sh
+++ scripts/run_exp3.sh
@@ -3,13 +3,13 @@
 num_eps=${1:-3}
 
 echo "Repeating experiment 1 for Agent 2"
-./scripts/run_exp1.sh $num_eps casc_agent_2_new
+./scripts/run_exp.sh $num_eps casc_agent_2_new
 
 echo "Repeating experiment 1 for Agent 3"
-./scripts/run_exp1.sh $num_eps casc_agent_3_new
+./scripts/run_exp.sh $num_eps casc_agent_3_new
 
 echo "Repeating experiment 1 for Agent 4"
-./scripts/run_exp1.sh $num_eps casc_agent_4_new
+./scripts/run_exp.sh $num_eps casc_agent_4_new
 
 echo "Generating csv files for every model"
 python ./scripts/plots/compute_all_stats.py --model_name casc_agent_1_new
